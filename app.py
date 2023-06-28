1eb1665fba7d8fc3a931d63277f6d59729f05ed5
Agamdeep Singh <agamdeep8467@gmail.com>
2023-06-29 01:00:11 +0530

    templates completed and app.py little bit

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ import numpy as np
 model = pickle.load(open('Model1.pkl','rb'))
 
 app = Flask(__name__)
+
+@app.route('/')
+def fraud():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods = ['POST'])
+def home():
